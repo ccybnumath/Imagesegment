146d1c53345dfa7dd768bacdf3c1b6bec0ff984d
unknown <710969718@qq.com>
2018-12-20 19:12:38 +0800

    add ImageGibbs.R containing kmeans

---
+++ ImageGibbs.R
@@ -0,0 +1,36 @@
+library(jpeg)
+img = readJPEG("./seg.jpg")
+img_seg = img
+
+K = 5 #number of clusters
+R=img[,,1]
+G=img[,,2]
+B=img[,,3]
+m = nrow(R)
+n = ncol(R)
+
+dim(R) = c(m*n,1)
+dim(G) = c(m*n,1)
+dim(B) = c(m*n,1)
+
+data = cbind(R,G,B)
+
+Kmeans = kmeans(data,K) #kmeans 5 clusters
+C = Kmeans$cluster
+dim(C) = c(m,n) #!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+for(i in 1:m){
+  for(j in 1:n){
+    k = C[i,j]
+    img_seg[i,j,] = Kmeans$centers[k,]
+  }
+}
+
+Sigma_bar = array(1:(9*K),dim = c(3,3,K))
+for(k in 1:K){
+  C1 = data[Kmeans$cluster==k,]
+  Sigma_bar[,,k]=cov(C1)
+}
+
+
+writeJPEG(img_seg,"./my1.jpg",0.95)
