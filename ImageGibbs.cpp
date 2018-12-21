69af081a52962432d1980ce852b9852341fdf109
unknown <710969718@qq.com>
2018-12-21 16:34:47 +0800

    firt version

--- ImageGibbs.cpp
+++ ImageGibbs.cpp
@@ -38,11 +38,41 @@ bool kMeans(mat &C, cube &P, unsigned int K,int n_iter){
  return status; 
 }
 
+int mirrorIndex(int fetchI, int length){
+  return (fetchI<0)+(fetchI>=length)*(length-2);
+}
+
+
+//Potts Model
+double Pr(mat &C, double alpha, double beta, int i, int j, int k){
+  vector<int> a{-1,1};
+  int m=0;
+  double sum=0;
+  for(m=0;m<1;m++)
+    sum+=(C.at(mirrorIndex(i+a.at(m),C.n_rows),j)==k?alpha:beta);
+  return exp(sum);
+}
+
+
+mat computeSk(cube &P, mat &C, mat &Mu, int k){
+  mat Sk(3,3,fill::zeros);
+  vec temp;
+  for(unsigned i=0;i<C.n_rows;i++)
+    for(unsigned j=0;j<C.n_cols;j++){
+      if(C.at(i,j)==k)
+        temp=P.tube(i,j);
+        temp-=Mu.col(k);
+        Sk+=temp*temp.t();
+    }
+  
+  return Sk;
+}
+
 
 
 // [[Rcpp::export]]
-mat ImageGibbs(int K, cube P, mat C, mat MuK, cube SigmaK, double alpha, double beta, 
-                 vec mu0, mat lambda0, vec v0, mat sigma0, int burnIn,int mcmcN){
+mat ImageGibbs(int K, cube P, mat C, mat Mu, cube Sigma, double alpha, double beta, 
+                 vec mu0, mat lambda0, int v0, mat sigma0, int burnIn,int mcmcN){
   /*
    * * INPUT:
    * K represents K groups
@@ -57,29 +87,51 @@ mat ImageGibbs(int K, cube P, mat C, mat MuK, cube SigmaK, double alpha, double 
    * sample
    */
   //Init
-  unsigned int i,j,k;
+  unsigned int i,j,k,l;
   int m=P.n_rows,n=P.n_cols;
   cube sampleC(m,n,mcmcN);
-  cube Sigma(3,3,K);
-  mat Mu(3,K);
-  Sigma=SigmaK;
-  Mu=MuK;
+  C=C-1;
   
-  mat mu1(3,1);
-  mat sigma1(3,3);
+  mat tempMu(3,1);
+  mat tempSigma(3,3);
+  int tempnk=0;
+  
+  int tempv0=0;
+  mat tempS(3,3,fill::zeros);
+  mat tempSk(3,3,fill::zeros);
   //Update
-  for(i=0;i<burnIn+ mcmcN;i++){
+  for(l=0;i<burnIn+ mcmcN;i++){
     
     // update Mu
     for(k=0;k<K;k++){
-      
+      tempnk=sum(sum(C==k));
+      tempSigma=inv(tempnk*inv(Sigma.slice(k))+inv(lambda0));
+      tempMu=tempSigma*(inv(Sigma.slice(k))*sum(P(find(C==k)))+inv(lambda0)*mu0);
+      Mu.col(k)=rmvnorm(1,tempMu,tempSigma);
     }
     
+    //update Sigma
+    for(k=0;k<K;k++){
+      tempnk=sum(sum(C==k));
+      tempv0=v0+tempnk;
+      tempSk=computeSk(P,C,Mu,k);
+      tempSk=inv(sigma0)+tempSk;
+      Sigma.slice(k)=riwish(tempv0,tempSk);
+    }
     
-    
-    
-    
-    
+    //update Cij
+    vec probK(K,fill::zeros);
+    vec fullvec = regspace<vec>(1,K);
+    for(i=0;i<m;i++)
+      for(j=0;j<n;j++){
+        for(k=0;k<K;k++){
+          probK.at(k)=sum(dmvnorm(P.tube(i,j),Mu.col(k),Sigma.slice(k)))*Pr(C,alpha,beta,i,j,k);
+        }
+        probK/=sum(probK);
+        C.at(i,j)=sum(Rcpp::RcppArmadillo::sample(fullvec,1,true,probK));
+      }
+    //record
+    if(l>=burnIn) sampleC.slice(l-burnIn)=C;
   }
   
   return sampleC;
