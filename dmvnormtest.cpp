b545b73ec6efaaed9b34b5779eadf4fc53a6f4d2
chencanyi <24704141@qq.com>
2019-01-04 20:16:36 +0800

    fix

---
+++ dmvnormtest.cpp
@@ -0,0 +1,50 @@
+#include <RcppArmadillo.h>
+#include <RcppArmadilloExtensions/sample.h>
+#include <mvnorm.h>
+#include <omp.h>
+#include <trng/discrete_dist.hpp>
+#include <trng/yarn2.hpp>
+#include <wishart.h>
+
+
+
+using namespace arma;
+using namespace std;
+// [[Rcpp::plugins(openmp)]]
+// [[Rcpp::depends(RcppArmadillo,RcppDist)]]
+
+// [[Rcpp::export]]
+void f(cube &P,mat &Mu,cube &Sigma){
+  int i,j;
+  int m = P.n_rows;
+  int n = P.n_cols;
+#pragma omp parallel for schedule(static) private(i, j) shared(m, n,P,Mu,Sigma)
+  for (i = 0; i < m; i++)
+  {
+    for (j = 0; j < n; j++)
+    {
+      dmvnorm(P.tube(i, j), Mu.col(1), Sigma.slice(1));
+    }
+  }
+}
+
+// [[Rcpp::export]]
+void g(cube &P,mat &Mu,cube &Sigma){
+ double x;
+  int i,j;
+  int m = P.n_rows;
+  int n = P.n_cols;
+#pragma omp parallel for schedule(static) private(i, j) shared(m, n,P,Mu,Sigma)
+  for (i = 0; i < m; i++)
+  {
+    for (j = 0; j < n; j++)
+    {
+      x=sum(dmvnorm(P.tube(i, j), Mu.col(1), Sigma.slice(1)));
+    }
+  }
+}
+/*** R
+library(microbenchmark)
+microbenchmark(f(P,Mu,Sigma),g(P,Mu,Sigma),times = 1)
+*/
+
